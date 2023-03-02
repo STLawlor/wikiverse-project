d030a54c7de92a7ea8bd224e5609e4b0fe2b1bb5
Stacey Lawlor <stacey.tlawlor@outlook.com>
2023-03-02 16:37:08 +0000

    install/add page details view

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export const Page = (props) => {
+// TODO: Tidy Date Created?
 
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+export const Page = ({ page, setPageDetails }) => {
+  return (
+    <>
+      <h3>{ page.title }</h3>
+      <p>Author: {page.author.name}</p>
+      <p>Content: {page.content}</p>
+      <p>Date Created: {page.createdAt}</p>
+      {page.tags.map((tag, i) => {
+        return <p key={i}>#{tag.name}</p>;
+      })}
+      <button onClick = { () => setPageDetails() }>Back to Wiki List</button>
+    </>
+  );
+};
